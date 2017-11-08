6854eb564044fcc46eddbd9ed8c16e8dbf0d5e71
Ben Wallace <ben@Bens-MacBook-Pro.local>
2017-11-08 07:56:35 -0500

    finished proto of 8.py and currently working on 9 easy

---
+++ easy/8.py
@@ -0,0 +1,8 @@
+# write a program that will print the song "99 bottles of beer on the wall".
+# for extra credit, do not allow the program to print each loop on a new line.
+
+i=99
+
+while i > 10:
+    print str(i) + " bottles of beer on the wall "
+    i -= 1
